903d53311101f5c0d37d2caa739cc13d0ea27530
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-09-04 11:23:46 -0500

    started Order model with find all query. connected to postgres db

--- app/order/Order.js
+++ app/order/Order.js
@@ -1,3 +1,32 @@
 'use strict';
 
 var pg = require('pg');
+
+var url = 'postgress://localhost:5050/Northwind';
+
+function query (sql, cb) {
+  pg.connect(url, function (err, db, done) {
+    if (err) throw err;
+
+    db.query(sql, function (err, res) {
+      if (err) throw err;
+
+      cb(err, res.rows);
+    });
+
+    done();
+
+  });
+};
+
+
+Order.findAll = function (cb) {
+  query('SELECT * FROM Categories;', function (err, orders) {
+    if (err) throw err;
+    console.log(orders);
+  });
+};
+
+
+
+module.exports = Order;
--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,8 @@
 var express = require('express');
 var router = express.Router();
 
-// var order = require('./order/routes');
+var order = require('./order/routes');
 
-// router.use('/', order);
+router.use('/', order);
 
 module.exports = router;
